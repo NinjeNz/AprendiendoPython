a81f9fc39d44b82e3a827d8994ec707dfc620672
NinjeNz <luis.uriasum@gmail.com>
2022-08-23 12:55:25 -0600

    Ejercicios print e input

---
+++ String.py
@@ -0,0 +1,18 @@
+print('Hola ' + 'Luis')
+print("Me llamo \"Luis\"")
+print("Esta es una linea\nY esta es otra linea")
+print("\tEsta es la tercera linea")
+print('\n')
+print('This isn\'t a number')
+print('Este signo \\ es una barra invertida')
+
+print("\nLínea 1\nLínea 2\nLínea 3")
+print('\n')
+
+print('A\tB\tC')
+print('D\tE\tF')
+print('G\tH\tI')
+print('\n')
+
+print('Barra Normal: /')
+print('Barra Invertida: \\')
